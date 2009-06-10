076e04eb87760e8ac80e9077bd38419387a14cd6
philomory <philomory@localhost>
2009-06-09 21:51:59 -0700

    Added some exception handling around MainWindow#draw, and began commenting my code in a more useful manner.

--- app/MainWindow.rb
+++ app/MainWindow.rb
@@ -3,10 +3,7 @@ require 'Platform'
 require 'Settings'
 
 require 'gosu'
-#unless RUBY_VERSION < "1.9.0"
-  require 'texplay'
-#end
-#require 'singleton'
+require 'texplay'
 require 'helper'
 
 require 'Constants'
@@ -26,10 +23,15 @@ require 'HighScore/Manager'
 
 module OperationLambda
   class MainWindow < Gosu::Window
-    #include Singleton
+
     attr_accessor :current_screen
     attr_reader :main_menu
 
+
+    # I'm implimenting Singleton here myself rather than using the Singleton
+    # module in the Ruby Standard Library, because the Ruby Standard Library
+    # version doesn't behave quite the way I need it to. For one of the things
+    # it does 'wrong', see http://www.ruby-forum.com/topic/179676
     private_class_method :new
     def self.instance
       unless @__instance__
@@ -39,8 +41,9 @@ module OperationLambda
     end
 
     def initialize
-      # Work around a weird Singleton recursion 'bug'. See http://www.ruby-forum.com/topic/179676
-      self.class.instance_variable_set(:@__instance__,self) 
+      # More Singleton stuff.
+      self.class.instance_variable_set(:@__instance__,self)
+       
       super(Sizes::WindowWidth, Sizes::WindowHeight,Settings[:fullscreen])
       self.caption = "Operation Lambda"
       ImageManager.load_images
@@ -55,9 +58,6 @@ module OperationLambda
     
     def current_screen=(screen)
       @current_screen = FadeSequence.new(@current_screen,screen)
-      #self.fade_to(screen)
-      #@current_screen = screen
-      #@current_screen.select_first if @current_screen.respond_to?(:select_first)
     end
     
     def immediate_set_screen(screen)
@@ -72,7 +72,17 @@ module OperationLambda
     end
     
     def draw
-      @current_screen.draw
+      # Because draw is utterly without side-effects (in terms of game state;
+      # obviously it has the 'side effect' of placing an image on the screen),
+      # there should be no risk in silently catching and discarding exceptions
+      # during the call to draw. Of course, it's better during development to
+      # have them around so that they point out bugs, but for the user it's
+      # better to have a single munged frame than to have the whole app crash
+      # just because I passed bad arguments to some draw function.
+      #
+      # In the future, though, I hope to throw some intelligent crash-logging
+      # into the picture.
+      @current_screen.draw rescue nil
       self.caption = "Operation Lambda: #{@fps.fps} frames per second." 
       # TODO: Do cool caption stuff; give specific titles depending on current screen!
     end
@@ -85,10 +95,16 @@ module OperationLambda
       @current_screen.button_up(id)
     end
     
+    # This method seems a little stupid. Why aren't I just calling 'close'
+    # directly? Frankly, I can't remember, and if I can't figure it out soon,
+    # I'm going to ditch it.
     def quit
       close
     end
     
+    # MainWindow is a singleton. This allows me to call methods on MainWindow
+    # that I really want to sent to MainWindow.instance, cutting out a lot of
+    # useless verbosity. And it avoids ill-performing method_missing hacks.
     (MainWindow.public_instance_methods - MainWindow.public_methods).each do |meth|
       (class << MainWindow; self; end).class_eval do
         define_method(meth) do |*args|
